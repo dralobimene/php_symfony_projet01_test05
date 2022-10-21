525f443e400b33d3bd02e8b8bb57edfb8269764c
dralobimene <P2MTC@fmx.fr>
2022-10-21 11:23:43 +0200

    upload du test06, affiche 1 Event de la DB sur la page accueil et aussi sur 1 page dédiée

--- src/Controller/CoreController.php
+++ src/Controller/CoreController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\EventRepository;
 
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,28 @@ class CoreController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function homepage(EventRepository $eventRepository): Response
     {
-        $events = $eventRepository->findAll();
-
-        return $this->render('core/index.html.twig', ['events' => $events]);
+	    // PR SELECTIONNER LES AFFICHAGES:
+	    // 01: utilisat° de la fct° findAll()
+	    // de la class
+	    // src/Repository/EventRepository.php
+	    // affiche ts les Events
+	    // 02: on la remplace par la methode
+	    // findBy qui prend 4 parametres:
+	    // - tab associatif qui represente la condit°
+	    //	= à la clause WHERE
+	    // - tab associatif = ORDER BY
+	    // - definit le nbre d'elts a recuperer = LIMIT
+	    // - indice a partir duqel on souhaite lire
+	    //	= à OFFSET
+	    // 01:
+	    // $events = $eventRepository->findAll();
+	    // 02:
+	    $events =
+		    $eventRepository->findBy(['isPublished' => true],
+		    				['eventDate' => 'ASC'],
+		    				12,
+		    				0);
+	    return $this->render('core/index.html.twig', ['events' => $events]);
     }
 
 }
--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+// personnel
+// ajout perso
 use App\Entity\Event;
 use App\Entity\Tag;
+use App\Repository\EventRepository;
+
+// built-in
+// ajout perso
 use DateInterval;
 use DateTime;
 use DateTimeImmutable;
 
+// built-in
+// ajout perso
 use Doctrine\ORM\EntityManagerInterface;
 
+// built-in
+// ajout automatique à la creat° du fichier controller
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,12 +28,20 @@ class EventController extends AbstractController {
 
 	/**
 	* @Route("/events/create", name="create_event")
-	*/
+	 */
+	// on emprunte cette Route par la barre d'adresse du
+	// nav
+
+	// controller qui permet d'ajouter des objets de
+	// type Event ds la table adéquate de la DB
+	// methode appliquée: injection d'1 instance de la
+	// class EntityManagerInterface
+	// NE PAS OUBLIER LE use NECESSAIRE
 	public function createEvent(EntityManagerInterface $entityManager): Response {
-	
+
 		// on crée un événement, ces données pourraient venir d'un formulaire
 		$event = new Event();
-		$event->setPicture('ADRESSE_D_1_IMAGE');
+		$event->setPicture('1.png');
 		$event->setTitle('À la découverte du développement web');
 		$event->setAddress('Sacré Coeur 3 VDN, Dakar');
 		$event->setDescription('DU TEXTE PR LA DERSCIPTION');
@@ -34,7 +52,6 @@ class EventController extends AbstractController {
 
 		// on crée un deuxième événement qui ne sera pas publié pour l'instant
 		$event2 = new Event();
-		// on renseigne seulement le titre qui est obligatoire
 		$event2->setTitle('Événement à venir, pas encore publique');
 
 		// on ajoute quelques tags à l'événement
@@ -47,14 +64,30 @@ class EventController extends AbstractController {
 		$event->addTag($codeTag);
 
 		/* on confie l'objet $event au gestionnaire d'entités,
-		    l'objet n'est pas encore enregistrer en base de données */
+			l'objet n'est pas encore enregistré en base de données
+		 	c'est la persistence de l'objet
+		 */
 		$entityManager->persist($event);
 
 		// on confie aussi l'objet $event2 au gestionnaire d'entités
+		// egalement persistence
 		$entityManager->persist($event2);
 
 		/* on exécute maintenant les 2 requêtes qui vont ajouter
-		    les objets $event et $event2 en base de données */
+		 * les objets $event et $event2 en base de données
+		 * ici normalement, je pense qu'il faudrait avt de
+		 * flusher, persister nos instances webTag et
+		 * codeTag
+		 * il y a 1 autre maniere que l'on a
+		 * appliqué:
+		 * rajouter 1 attribut "cascade" ds la class
+		 * Event,
+		 * au niveau de la relation entre Event et
+		 * Tag
+		 * pr dire a doctrine de gerer les 2 flush
+		 * restants
+		 * voir src/Entity/Event.php
+		 * */
 		$entityManager->flush();
 
 		return new Response(
@@ -63,16 +96,20 @@ class EventController extends AbstractController {
 		);
 	}
 
-	/**
-	* @Route("/events/{id}", name="show_event", requirements={"id"="\d+"})
-	*/
-	public function show($id): Response {
-		return $this->render('event/show.html.twig', ['event_id' => $id]);
+	// Route dynamique qui permet l'affichage d'1 Event
+	// spécifique ds sa page dédiée
+	// NE PAS OUBLIER L'IMPORT
+	#[Route('/events/{id}', name: 'show_event', requirements : ['id' => '\d+'])]
+	public function show($id, EventRepository $eventRepository): Response {
+		$event = $eventRepository->find($id);
+		return $this->render('event/show.html.twig', ['event' => $event]);
 	}
 
-	/**
-	* @Route("/events/{category}", name="list_events")
-	*/
+	// Route dynamique qui permet l'affichage d'1
+	// category spécifique
+	// on y accede par la barre d'adresse du nav
+	// aucun template twig, codage en dur
+	#[Route("/events/{category}", name:"list_events")]
 	public function list($category = null): Response {
 		$htmlMessage = "<h1>Liste des événements";
 		if ($category) {
@@ -82,5 +119,5 @@ class EventController extends AbstractController {
 
 		return new Response($htmlMessage);
 	}
-	
+
 }
